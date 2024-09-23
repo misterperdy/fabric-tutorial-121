4bb9104cbe6ef0ed0b5816603e00ebd141dcd9ff
misterperdy <mateialexanto@gmail.com>
2024-09-23 16:32:01 +0300

    Initial commit

---
+++ src/main/java/com/example/azlo/modtest/ModTest.java
@@ -0,0 +1,15 @@
+package com.example.azlo.modtest;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModTest implements ModInitializer {
+	public static final String MOD_ID = "azlomod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/example/azlo/modtest/ModTestClient.java
@@ -0,0 +1,11 @@
+package com.example.azlo.modtest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTestClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
